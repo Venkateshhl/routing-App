3ca30735b928e368852390a2b675d13df7d768a3
venkateshhl1995 <venkateshhl1995@gmail.com>
2023-08-30 07:56:03 +0000

    solution

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+import {Component} from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div className="about-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+          alt="about"
+          className="about-img"
+        />
+        <h1 className="about-heading">About</h1>
+        <p className="about-paragraph">
+          My name is venkatesh and I love to create! I am a frontend web developer
+        </p>
+      </div>
+    )
+  }
+}
+
+export default About
